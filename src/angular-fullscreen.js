ef6555307669e2c932ecbffd0297c3924c675f5a
Nikita Korotaev <websirnik@gmail.com>
2019-01-08 12:39:31 +0000

    Remove outdated option

--- src/angular-fullscreen.js
+++ src/angular-fullscreen.js
@@ -31,7 +31,7 @@
                   if (/Version\/[\d]{1,2}(\.[\d]{1,2}){1}(\.(\d){1,2}){0,1} Safari/.test(navigator.userAgent)) {
                      element.webkitRequestFullscreen();
                   } else {
-                     element.webkitRequestFullscreen(isKeyboardAvailbleOnFullScreen);
+                     element.webkitRequestFullscreen();
                   }
                } else if (element.msRequestFullscreen) {
                   element.msRequestFullscreen();
